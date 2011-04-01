3a85f6632a4ac6c12ae71e07fa14087981570457
dnadasi@google.com <dnadasi@google.com@efc0f027-fe5b-7ab6-0a26-bc33fd47a49b>
2011-04-01 03:30:04 +0000

    Show selection text in Create Task menu item

--- menus.js
+++ menus.js
@@ -1,29 +1,28 @@
 // Copyright 2011, the Google Tasks Chrome Extension authors.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the 'License');
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //    http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an 'AS IS' BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 function addTaskClickHandler(info, tab) {
-  var text = info["selectionText"];
-  var notes = info["pageUrl"]
+  var text = info['selectionText'];
+  var notes = info['pageUrl']
   addTask({
       'title': text,
       'notes': notes
   });
 }
 
-// Create one test item for each context type.
-var contexts = ["selection"];
-var context = "selection";
-var title = "Create Task from selection"
-var id = chrome.contextMenus.create({"title": title, "contexts": ["selection"], "onclick": addTaskClickHandler});
-console.log("'" + context + "' item:" + id);
+chrome.contextMenus.create({
+  'title': 'Create Task for \'%s\'',
+  'contexts': ['selection'],
+  'onclick': addTaskClickHandler
+});
