d3c0424d50b79a9e73d04818de8297b7af002ad1
dnadasi@google.com <dnadasi@google.com@efc0f027-fe5b-7ab6-0a26-bc33fd47a49b>
2011-04-01 03:08:55 +0000

    Add URL as task note.
    Also, clean up tasksapi.js. Also also make notifications fade.

--- trunk/menus.js
+++ trunk/menus.js
@@ -12,26 +12,18 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// A generic onclick callback function.
-function genericOnClick(info, tab) {
-  console.log("item " + info.menuItemId + " was clicked");
-  console.log("info: " + JSON.stringify(info));
-  console.log("tab: " + JSON.stringify(tab));
-  console.log("task: " + info["selectionText"]);
-}
-
-function temporaryAddTaskClickHandler(info, tab) {
-  console.log("item " + info.menuItemId + " was clicked");
-  console.log("info: " + JSON.stringify(info));
-  console.log("tab: " + JSON.stringify(tab));
+function addTaskClickHandler(info, tab) {
   var text = info["selectionText"];
-  // var notes = info["pageUrl"]
-  addTask({title: text});
+  var notes = info["pageUrl"]
+  addTask({
+      'title': text,
+      'notes': notes
+  });
 }
 
 // Create one test item for each context type.
 var contexts = ["selection"];
 var context = "selection";
 var title = "Create Task from selection"
-var id = chrome.contextMenus.create({"title": title, "contexts": ["selection"], "onclick": temporaryAddTaskClickHandler});
+var id = chrome.contextMenus.create({"title": title, "contexts": ["selection"], "onclick": addTaskClickHandler});
 console.log("'" + context + "' item:" + id);
